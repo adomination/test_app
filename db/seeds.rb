aef88ea4353e57a762ace1f9d107764aa36ae1c9
AD <mcad23@gmail.com>
2019-09-04 21:03:39 +0300

    [#Feature_6] Add edit for customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ customer_attributes = [
   { name: 'Dmitry', phone: '+7-567-234-23-34', description: 'second customer' },
   { name: 'Sergey', phone: '+7-945-567-45-56', description: 'third customer' },
   { name: 'Jhon', phone: '+7-456-234-56-67', description: 'fourth customer' },
-  { name: 'Liza', phone: '+7-245-687-67-23', description: 'fifth customer' }
+  { name: 'Liza', phone: '+7-245-687-67-23', description: 'fifth customer' },
+  { name: 'Anna', phone: '+7-333-555-23-21', description: 'sixth customer' }
 ]
 
 customer_attributes.each do |attr|
